aedf23b991aa547c4a10bfa5f158f8972833536f
DaibhidhBoyle <Daibhidh_Boyle@gmail.com>
2019-05-22 17:26:19 +0100

    WHisky lab pre-mvp

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepository.java
@@ -0,0 +1,19 @@
+package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DistilleryRepository extends JpaRepository<Distillery, Long>, DistilleryRepositoryCustom {
+
+   List<Distillery> findDistilleryByRegion(String region);
+
+
+
+
+}
